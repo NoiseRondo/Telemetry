3fdb88fc4d1bab8f9e9aea14576bcab5c71bca0d
Vladimir Glukharev <darknosiernondo@gmail.com>
2022-02-12 13:05:05 +0300

    Задача 1 решена

--- Zadanie_1/rabota1.py
+++ Zadanie_1/rabota1.py
@@ -1,7 +1,22 @@
 number1 = input('Введите первое число:')
 number2 = input('Введите второе число:')
 
-
 summa = number1 + number2
 
 print(summa)
+
+
+number3 = int(input('Введите третье число:'))
+
+if number3 > 10:
+    print('Ваше число больше 10')
+else:
+    print('Ваше число меньше 10')
+    number4 = 0
+
+    while number4 < 10:
+        number5 = number4 + number3
+        print(number4)
+        number4+=1
+        if number4 == 10:
+            print('Ваше число стало равно 10')
