4141e6dfb954699294b60e5457fe6cde391178d2
vlgl <darknoiserondo@gmail.com>
2022-02-19 18:46:31 +0300

    задача 3 выполнена

--- Zadanie_2/task3.py
+++ Zadanie_2/task3.py
@@ -1,21 +1,33 @@
 
-
 month = {
-    1: "January, Winter",
-    2: "February, Winter",
-    3: "March, Spring",
-    4: "April, Spring",
-    5: "May, Spring",
-    6: "June, Summer",
-    7: "July, Summer",
-    8: "August, Summer",
-    9: "September, Fall",
-    10: "October, Fall",
-    11: "November, Fall",
-    12: "December, Winter"
+    1: "January",
+    2: "February",
+    3: "March",
+    4: "April",
+    5: "May",
+    6: "June",
+    7: "July",
+    8: "August",
+    9: "September",
+    10: "October",
+    11: "November",
+    12: "December"
 }
 
+sez = ["Winter", "Spring", "Summer", "Fall"]
+
 mes = int(input("Введите месяц: "))
+if mes == 1 or mes == 2 or mes == 12:
+    print(sez[0])
+elif mes == 3 or mes == 4 or mes == 5:
+    print(sez[1])
+elif mes == 6 or mes == 7 or mes == 8:
+    print(sez[2])
+elif mes == 9 or mes == 10 or mes == 11:
+    print(sez[3])
+else:
+    print("Нет такого месяца")
+
 print(month[mes])
 
 
