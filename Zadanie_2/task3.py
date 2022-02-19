a761cf35392aeb1f72b372637f018fdf55da926d
vlgl <darknoiserondo@gmail.com>
2022-02-19 18:30:42 +0300

    задача 3 выполнена

---
+++ Zadanie_2/task3.py
@@ -0,0 +1,41 @@
+
+
+month = {
+    1: "January, Winter",
+    2: "February, Winter",
+    3: "March, Spring",
+    4: "April, Spring",
+    5: "May, Spring",
+    6: "June, Summer",
+    7: "July, Summer",
+    8: "August, Summer",
+    9: "September, Fall",
+    10: "October, Fall",
+    11: "November, Fall",
+    12: "December, Winter"
+}
+
+mes = int(input("Введите месяц: "))
+print(month[mes])
+
+
+
+#year_seas = {
+    #"Winter": ['December', 'January', 'February'],
+    #"Spring": ['March', 'April', "May"],
+    #"Summer": ['June', 'July', 'August'],
+    #"Fall": ['September', 'October', 'November']
+#}
+
+#"January": 1,
+    #"February": 2,
+    #"March": 3,
+    #"April": 4,
+    #"May": 5,
+    #"June": 6,
+    #"July": 7,
+    #"August": 8,
+    #"September": 9,
+    #"October": 10,
+    #"November": 11,
+    #"December": 12,
